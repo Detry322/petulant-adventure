93552628bf0081a2dd18823dfe58ed81c747fd77
Jack Serrino <Jserrino@gmail.com>
2014-10-28 14:46:57 -0400

    Add GameCenterController to handle all GameCenter Interaction

---
+++ Petulant Adventure/GameCenterController.h
@@ -0,0 +1,13 @@
+//
+//  GameCenterController.h
+//  Petulant Adventure
+//
+//  Created by Jack Serrino on 10/28/14.
+//  Copyright (c) 2014 Jack Serrino. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GameCenterController : NSObject
+
+@end
---
+++ Petulant Adventure/GameCenterController.m
@@ -0,0 +1,13 @@
+//
+//  GameCenterController.m
+//  Petulant Adventure
+//
+//  Created by Jack Serrino on 10/28/14.
+//  Copyright (c) 2014 Jack Serrino. All rights reserved.
+//
+
+#import "GameCenterController.h"
+
+@implementation GameCenterController
+
+@end
