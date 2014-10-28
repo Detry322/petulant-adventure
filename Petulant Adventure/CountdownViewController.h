72dc311c2459375958db68a03fb566361012a7f8
Jack Serrino <Jserrino@gmail.com>
2014-10-28 02:02:33 -0400

    Add the CountdownViewController to manage the countdown screen

---
+++ Petulant Adventure/CountdownViewController.h
@@ -0,0 +1,13 @@
+//
+//  CountdownViewController.h
+//  Petulant Adventure
+//
+//  Created by Jack Serrino on 10/28/14.
+//  Copyright (c) 2014 Jack Serrino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CountdownViewController : UIViewController
+
+@end
