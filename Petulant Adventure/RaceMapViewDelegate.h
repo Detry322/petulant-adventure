4e38bd59b05910390f1bea34f5212d3de743936d
Jack Serrino <Jserrino@gmail.com>
2014-10-28 16:35:21 -0400

    Add RaceMapViewDelegate protocol to tell implementing views when to update

---
+++ Petulant Adventure/RaceMapViewDelegate.h
@@ -0,0 +1,20 @@
+//
+//  RaceMapViewDelegate.h
+//  Petulant Adventure
+//
+//  Created by Jack Serrino on 10/28/14.
+//  Copyright (c) 2014 Jack Serrino. All rights reserved.
+//
+
+#ifndef Petulant_Adventure_RaceMapViewDelegate_h
+#define Petulant_Adventure_RaceMapViewDelegate_h
+
+@protocol RaceMapViewDelegate
+
+-(void)redrawPlayer:(NSString *)identifier;
+@optional
+-(void)redrawDestination;
+
+@end
+
+#endif
