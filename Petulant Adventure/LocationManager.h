7a4b0287813f53881bb97ca08cd8bbddb6bb96f1
Jack Serrino <Jserrino@gmail.com>
2014-10-28 16:37:33 -0400

    Add LocationManager singleton class to interface with CoreLocation

---
+++ Petulant Adventure/LocationManager.h
@@ -0,0 +1,13 @@
+//
+//  LocationManager.h
+//  Petulant Adventure
+//
+//  Created by Jack Serrino on 10/28/14.
+//  Copyright (c) 2014 Jack Serrino. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LocationManager : NSObject
+
+@end
---
+++ Petulant Adventure/LocationManager.m
@@ -0,0 +1,15 @@
+
+
+//
+//  LocationManager.m
+//  Petulant Adventure
+//
+//  Created by Jack Serrino on 10/28/14.
+//  Copyright (c) 2014 Jack Serrino. All rights reserved.
+//
+
+#import "LocationManager.h"
+
+@implementation LocationManager
+
+@end
