21bd232965cffb50cf2527540d9f583d7964014a
Jack Serrino <Jserrino@gmail.com>
2014-10-28 02:00:58 -0400

    Add the RaceLobbyViewController to track players in the lobby

---
+++ Petulant Adventure/RaceLobbyViewController.h
@@ -0,0 +1,13 @@
+//
+//  RaceLobbyViewController.h
+//  Petulant Adventure
+//
+//  Created by Jack Serrino on 10/28/14.
+//  Copyright (c) 2014 Jack Serrino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RaceLobbyViewController : UIViewController
+
+@end
