cc63250ada9a70ed4c3cc7b0d7dd36475988c967
Jack Serrino <Jserrino@gmail.com>
2014-10-28 14:46:28 -0400

    Add RaceController to handle all game functions

---
+++ Petulant Adventure/RaceController.h
@@ -0,0 +1,13 @@
+//
+//  RaceController.h
+//  Petulant Adventure
+//
+//  Created by Jack Serrino on 10/28/14.
+//  Copyright (c) 2014 Jack Serrino. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RaceController : NSObject
+
+@end
---
+++ Petulant Adventure/RaceController.m
@@ -0,0 +1,13 @@
+//
+//  RaceController.m
+//  Petulant Adventure
+//
+//  Created by Jack Serrino on 10/28/14.
+//  Copyright (c) 2014 Jack Serrino. All rights reserved.
+//
+
+#import "RaceController.h"
+
+@implementation RaceController
+
+@end
