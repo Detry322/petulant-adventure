02d32900f9d257047351288c2f1dec2dbb6eb63c
Jack Serrino <Jserrino@gmail.com>
2014-10-29 02:37:59 -0400

    Add Game Center Delegate methods to GameCenterController

--- Petulant Adventure/GameCenterController.h
+++ Petulant Adventure/GameCenterController.h
@@ -7,15 +7,35 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <GameKit/GameKit.h>
 
-@interface GameCenterController : NSObject
+@interface GameCenterController : NSObject <GKMatchmakerViewControllerDelegate, GKMatchDelegate>
 
 @property (readonly) BOOL loggedIn;
+@property (retain) UIViewController *containerViewController;
+@property (retain) GKMatch *match;
 
 + (GameCenterController *)sharedController;
 
--(void)authUser;
--(void)authStateChanged;
+- (void)authUser;
+- (void)authStateChanged;
+
+- (void)findMatchWithViewController:(UIViewController *)viewController;
+
+
+#pragma mark GKMatchmakerViewControllerDelegate
+
+// The user has cancelled matchmaking
+- (void)matchmakerViewControllerWasCancelled:(GKMatchmakerViewController *)viewController;
+- (void)matchmakerViewController:(GKMatchmakerViewController *)viewController didFailWithError:(NSError *)error;
+- (void)matchmakerViewController:(GKMatchmakerViewController *)viewController didFindMatch:(GKMatch *)theMatch;
+
+#pragma mark GKMatchDelegate
+
+- (void)match:(GKMatch *)theMatch didReceiveData:(NSData *)data fromRemotePlayer:(NSString *)playerID;
+- (void)match:(GKMatch *)theMatch player:(NSString *)playerID didChangeConnectionState:(GKPlayerConnectionState)state;
+- (void)match:(GKMatch *)theMatch connectionWithPlayerFailed:(NSString *)playerID withError:(NSError *)error;
+- (void)match:(GKMatch *)theMatch didFailWithError:(NSError *)error;
 
 
 @end
--- Petulant Adventure/GameCenterController.m
+++ Petulant Adventure/GameCenterController.m
@@ -7,7 +7,6 @@
 //
 
 #import "GameCenterController.h"
-#import <GameKit/GameKit.h>
 
 @implementation GameCenterController
 
@@ -20,11 +19,11 @@ static GameCenterController *controller = nil;
     return controller;
 }
 
--(void)authStateChanged {
+- (void)authStateChanged {
     _loggedIn = [GKLocalPlayer localPlayer].isAuthenticated;
 }
 
--(void)authUser {
+- (void)authUser {
     if (!_loggedIn) {
         NSLog(@"Authenticating...");
         [[GKLocalPlayer localPlayer] authenticateWithCompletionHandler:nil];
@@ -33,6 +32,94 @@ static GameCenterController *controller = nil;
     {
         NSLog(@"Already Authed");
     }
+}
+
+
+
+#pragma mark GKMatchmakerViewControllerDelegate
+
+// The user has cancelled matchmaking
+- (void)matchmakerViewControllerWasCancelled:(GKMatchmakerViewController *)viewController {
+    [_containerViewController dismissModalViewControllerAnimated:YES];
+    NSLog(@"User cancelled matchmaking");
+}
+
+// Matchmaking has failed with an error
+- (void)matchmakerViewController:(GKMatchmakerViewController *)viewController didFailWithError:(NSError *)error {
+    [_containerViewController dismissModalViewControllerAnimated:YES];
+    NSLog(@"Error finding match: %@", error.localizedDescription);
+}
+
+// A peer-to-peer match has been found, the game should start
+- (void)matchmakerViewController:(GKMatchmakerViewController *)viewController didFindMatch:(GKMatch *)theMatch {
+    [_containerViewController dismissModalViewControllerAnimated:YES];
+    _match = theMatch;
+    _match.delegate = self;
+    if (_match.expectedPlayerCount == 0) {
+        NSLog(@"Ready to start match!");
+        //TODO
+    }
+}
+
+- (void)findMatchWithViewController:(UIViewController *)viewController{
+    _match = nil;
+    _containerViewController = viewController;
+    [viewController dismissModalViewControllerAnimated:NO];
+    
+    GKMatchRequest *request = [[GKMatchRequest alloc] init];
+    request.minPlayers = 2;
+    request.maxPlayers = 2;
+    
+    GKMatchmakerViewController *mmvc =
+    [[GKMatchmakerViewController alloc] initWithMatchRequest:request];
+    mmvc.matchmakerDelegate = self;
+    
+    [viewController presentModalViewController:mmvc animated:YES];
+}
+
+#pragma mark GKMatchDelegate
+
+// The match received data sent from the player.
+- (void)match:(GKMatch *)theMatch didReceiveData:(NSData *)data fromRemotePlayer:(NSString *)playerID {
+    if (_match != theMatch) return;
+    //This is really complicated and will take time.
+}
+
+- (void)match:(GKMatch *)theMatch player:(NSString *)playerID didChangeConnectionState:(GKPlayerConnectionState)state {
+    if (_match != theMatch) return;
+    
+    switch (state) {
+        case GKPlayerStateConnected:
+            // handle a new player connection.
+            NSLog(@"Player connected!");
+            
+            if (theMatch.expectedPlayerCount == 0) {
+                NSLog(@"Ready to start match!");
+            }
+            
+            break;
+        case GKPlayerStateDisconnected:
+            // a player just disconnected.
+            NSLog(@"Player disconnected!");
+            //TODO
+            break;
+    }
+}
+
+// The match was unable to connect with the player due to an error.
+- (void)match:(GKMatch *)theMatch connectionWithPlayerFailed:(NSString *)playerID withError:(NSError *)error {
+    if (_match != theMatch) return;
+    
+    NSLog(@"Failed to connect to player with error: %@", error.localizedDescription);
+    //TODO
+}
+
+// The match was unable to be established with any players due to an error.
+- (void)match:(GKMatch *)theMatch didFailWithError:(NSError *)error {
+    if (_match != theMatch) return;
+    
+    NSLog(@"Match failed with error: %@", error.localizedDescription);
+    //TODO
 }
 
 @end
