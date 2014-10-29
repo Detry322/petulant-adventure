c8d2f50121d18c41eee2d382449501b1c547da9a
Jack Serrino <Jserrino@gmail.com>
2014-10-29 01:19:50 -0400

    Update race controller to be able to pass events regarding game flow

--- Petulant Adventure/RaceController.h
+++ Petulant Adventure/RaceController.h
@@ -9,12 +9,16 @@
 #import <Foundation/Foundation.h>
 #import <CoreLocation/CLLocation.h>
 #import "DirectionSet.h"
+#import "RaceDelegate.h"
+#import "RaceMapViewDelegate.h"
+#import "Player.h"
 
 typedef enum {
     RaceInLobby,
     RaceInProgress,
     RaceAtFinishLine,
-    RaceFinished
+    RaceFinished,
+    RaceError
 } RACE_STATE;
 
 @interface RaceController : NSObject
@@ -23,7 +27,32 @@ typedef enum {
 @property (readonly) RACE_STATE state;
 @property (readonly) CLLocation *destination;
 @property (readonly) DirectionSet *directions;
-@property (readonly) NSDictionary *players;
+@property (readonly) NSMutableDictionary *players;
+@property (readonly) NSDate *startDate;
+@property (readonly) NSString *localPlayerIdentifier;
+@property id<RaceMapViewDelegate> raceMapViewDelegate;
+@property id<RaceDelegate> raceDelegate;
 
++ (RaceController *)sharedController;
++ (void)createRace:(BOOL)isHost withLocalPlayerIdentifier:(NSString *)localPlayerIdentifier andDelegate:(id<RaceDelegate>)raceDelegate andMapViewDelegate:(id<RaceMapViewDelegate>)raceMapViewDelegate;
++ (void)cleanUp;
+
+- (id)init:(BOOL)isHost withLocalPlayerIdentifier:(NSString *)localPlayerIdentifier;
+
+- (void)startMatch;
+
+- (BOOL)isMatchReady;
+- (BOOL)didMatchStart;
+- (BOOL)didAllPlayersArrive;
+
+- (BOOL)addPlayerFromIdentifier:(NSString *)identifier;
+
+- (NSTimeInterval)playerCompletionTime:(NSString *)playerIdentifier;
+
+- (void)destinationUpdated:(CLLocation *)newDestination;
+- (BOOL)playerUpdated:(NSString *)playerIdentifier withLocation:(CLLocation *)newLocation;
+- (void)playerDisconnected:(NSString *)playerIdentifier;
+
+- (void)matchEndedWithReason:(MATCH_END_REASON)reason;
 
 @end
--- Petulant Adventure/RaceController.m
+++ Petulant Adventure/RaceController.m
@@ -10,4 +10,115 @@
 
 @implementation RaceController
 
+static RaceController *controller = nil;
++ (RaceController *)sharedController {
+    return controller;
+}
+
++ (void)createRace:(BOOL)isHost withLocalPlayerIdentifier:(NSString *)localPlayerIdentifier andDelegate:(id<RaceDelegate>)raceDelegate andMapViewDelegate:(id<RaceMapViewDelegate>)raceMapViewDelegate{
+    controller = [[RaceController alloc] init:isHost withLocalPlayerIdentifier:localPlayerIdentifier];
+    [controller setRaceDelegate:raceDelegate];
+    [controller setRaceMapViewDelegate:raceMapViewDelegate];
+}
+
++ (void)cleanUp {
+    controller = nil;
+}
+
+- (id)init:(BOOL) isHost withLocalPlayerIdentifier:(NSString *)localPlayerIdentifier{
+    if (self = [super init])
+    {
+        _players = [[NSMutableDictionary alloc] init];
+        _state = RaceInLobby;
+        _isHost = isHost;
+        _localPlayerIdentifier = localPlayerIdentifier;
+        [self addPlayerFromIdentifier:localPlayerIdentifier];
+        return self;
+    }
+    return nil;
+}
+
+
+- (void)startMatch {
+    _state = RaceInProgress;
+    _startDate = [NSDate date];
+    //@TODO
+}
+
+- (BOOL)didMatchStart {
+    return _state == RaceInProgress || _state == RaceAtFinishLine || _state == RaceFinished;
+}
+
+- (BOOL)isMatchReady {
+    NSEnumerator *enumerator = [_players keyEnumerator];
+    Player *localPlayer = _players[_localPlayerIdentifier];
+    id key;
+    while (key = [enumerator nextObject])
+    {
+        if (![localPlayer isCloseToOtherPlayer:_players[key]])
+            return NO;
+    }
+    return YES;
+}
+
+- (BOOL)didAllPlayersArrive {
+    NSEnumerator *enumerator = [_players keyEnumerator];
+    id key;
+    while (key = [enumerator nextObject])
+    {
+        if (![_players[key] arrived])
+            return NO;
+    }
+    return YES;
+}
+
+- (void)destinationUpdated:(CLLocation *)newDestination {
+    _destination = newDestination;
+    [_raceMapViewDelegate redrawDestination];
+}
+
+- (NSTimeInterval)playerCompletionTime:(NSString *)playerIdentifier {
+    Player *player = _players[playerIdentifier];
+    if ([player arrived])
+        return [[player arrivedTime] timeIntervalSinceDate:_startDate];
+    return -1.0;
+}
+
+- (BOOL)playerUpdated:(NSString *)playerIdentifier withLocation:(CLLocation *)newLocation {
+    BOOL didCreatePlayer = [self addPlayerFromIdentifier:playerIdentifier];
+    Player *player = _players[playerIdentifier];
+    [player setLocation:newLocation];
+    if ([self didMatchStart] && [player isAtDestination:_destination])
+    {
+        [player arrive];
+        if ([playerIdentifier isEqualToString:_localPlayerIdentifier])
+        {
+            [_raceDelegate localPlayerArrived];
+            _state = RaceAtFinishLine;
+        }
+        if ([self didAllPlayersArrive])
+        {
+            //End the match here
+        }
+    }
+    [_raceMapViewDelegate redrawPlayer:playerIdentifier];
+    return didCreatePlayer;
+}
+
+- (void)playerDisconnected:(NSString *)playerIdentifier{
+    [self matchEndedWithReason:PlayerDisconnected];
+}
+
+- (BOOL)addPlayerFromIdentifier:(NSString *)identifier {
+    if (_players[identifier])
+        return NO;
+    Player *newPlayer = [[Player alloc] initWithIdentifier:identifier];
+    _players[identifier] = newPlayer;
+    return YES;
+}
+
+- (void)matchEndedWithReason:(MATCH_END_REASON)reason {
+    [_raceDelegate matchEndedWithReason:reason];
+}
+
 @end
