5a12e27f44d8fc2568cc9e5357fdd806ec99fe94
Jack Serrino <Jserrino@gmail.com>
2014-10-30 00:32:22 -0400

    Implement all wrapper methods for LocationManager

--- Petulant Adventure/LocationManager.h
+++ Petulant Adventure/LocationManager.h
@@ -7,10 +7,21 @@
 //
 
 #import <Foundation/Foundation.h>
-#import <CoreLocation/CLLocation.h>
+#import <CoreLocation/CoreLocation.h>
+#import "LocationReceiver.h"
 
-@interface LocationManager : NSObject
+@interface LocationManager : NSObject <CLLocationManagerDelegate>
 
 + (CLLocation *)currentLocation;
++ (void)startTrackingLocationWithReceiver:(id<LocationReceiver>)receiver;
++ (void)stopTrackingLocation;
+
+@property id<LocationReceiver> messageReceiver;
+@property (readonly) CLLocationManager *manager;
+@property (readonly) CLLocation *lastLocation;
+
+- (id)initWithReceiver:(id<LocationReceiver>)receiver;
+- (id)init;
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations;
 
 @end
--- Petulant Adventure/LocationManager.m
+++ Petulant Adventure/LocationManager.m
@@ -12,4 +12,47 @@
 
 @implementation LocationManager
 
+static LocationManager *instance = nil;
+static CLLocationManager *manager = nil;
+
++ (CLLocation *)currentLocation {
+    return [instance lastLocation];
+}
+
++ (void)startTrackingLocationWithReceiver:(id<LocationReceiver>)receiver {
+    if (!instance)
+        instance = [[LocationManager alloc] initWithReceiver:receiver];
+    if (!manager)
+        manager = [[CLLocationManager alloc] init];
+    [manager setDelegate:instance];
+    [manager setDesiredAccuracy:kCLLocationAccuracyBest];
+    [manager setDistanceFilter:5];
+    [manager startUpdatingLocation];
+}
+
++ (void)stopTrackingLocation {
+    [manager stopUpdatingLocation];
+    instance = nil;
+    manager = nil;
+}
+
+- (id)initWithReceiver:(id<LocationReceiver>)receiver {
+    if (self = [super init])
+    {
+        [self setMessageReceiver:receiver];
+        return self;
+    }
+    return nil;
+}
+
+- (id)init {
+    return [super init];
+}
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations {
+    CLLocation *loc = [locations lastObject];
+    _lastLocation = loc;
+    [_messageReceiver receiveLocation:loc];
+}
+
 @end
